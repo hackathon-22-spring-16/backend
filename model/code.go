577254feb074405b7a6b5be674f8d7bebd8582d5
H1rono <hronok66@gmail.com>
2022-06-18 03:32:14 +0900

    Code 構造体を変更、 AddCode を実装

--- model/code.go
+++ model/code.go
@@ -2,6 +2,9 @@ package model
 
 import (
 	"context"
+	"errors"
+	"math/rand"
+	"time"
 )
 
 // | USER_NAME   | HASH    | PLAIN_CODE | STDIN    | TITLE       | OPTIONS  | CREATED_AT |
@@ -11,15 +14,55 @@ import (
 
 type Code struct {
 	UserName  string
-	Hash      string
 	PlainCode string
 	Stdin     string
 	Title     string
 	Options   string
 }
 
-func AddCode(ctx context.Context, code *Code) error {
-	return nil
+func makeHash() string {
+	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	rand.Seed(time.Now().UnixNano())
+	result := make([]byte, 8)
+	for i := range result {
+		result[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(result)
+}
+
+func AddCode(ctx context.Context, code *Code) (hash string, err error) {
+	for i := 0; i < 10; i++ {
+		h := makeHash()
+		c := 0
+		err := db.SelectContext(
+			ctx,
+			&c,
+			"SELECT COUNT(*) FORM codes WHERE `user_name` = ? AND `hash` = ?",
+			code.UserName,
+			h,
+		)
+		if err != nil {
+			return "", err
+		}
+		if c > 0 {
+			continue
+		}
+		_, err = db.ExecContext(
+			ctx,
+			"INSERT INTO codes (`user_name`, `hash`, `plain_code`, `stdin`, `title`, `options`) VALUES (?, ?, ?, ?, ?, ?) WHERE NOT EXIST",
+			code.UserName,
+			h,
+			code.PlainCode,
+			code.Stdin,
+			code.Title,
+			code.Options,
+		)
+		if err != nil {
+			return "", err
+		}
+		return h, nil
+	}
+	return "", errors.New("timeout while adding the code")
 }
 
 func GetCode(ctx context.Context, userName string, hash string) (*Code, error) {
