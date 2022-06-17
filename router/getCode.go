ccc02bf8af3c28afb8c1a0b35f97af345bed8f25
tatyam <tatuhito0125@yahoo.co.jp>
2022-06-18 00:08:50 +0900

    変数を削った構造体を作った

--- router/getCode.go
+++ router/getCode.go
@@ -7,6 +7,13 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type getCodeResponse struct {
+	PlainCode string `json:"plainCode"`
+	Stdin     string `json:"stdin"`
+	Title     string `json:"title"`
+	Options   string `json:"options"`
+}
+
 // GET /get-code/:username/:hash
 func GetCodeHandler(c echo.Context) error {
 	username := c.Param("username")
@@ -20,5 +27,5 @@ func GetCodeHandler(c echo.Context) error {
 	if code == nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, "Internal Server Error")
 	}
-	return c.JSON(200, &code)
+	return c.JSON(200, getCodeResponse{code.PlainCode, code.Stdin, code.Title, code.Options})
 }
